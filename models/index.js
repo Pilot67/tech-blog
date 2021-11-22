62b79fbe5f4c94b0d1f7fd24dbd94b6cb3e67fe8
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-23 09:46:26 +1100

    added models

---
+++ models/index.js
@@ -0,0 +1,34 @@
+const User = require("./User");
+const Posts = require("./Posts");
+const Comments = require("./Comments");
+
+User.hasMany(Posts, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Posts.belongsTo(User, {
+    foreignKey: 'id',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Comments, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+Comments.belongsTo(User, {
+    foreignKey: 'id',
+});
+
+Posts.hasMany(Comments, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+
+Comments.belongsTo(Posts, {
+    foreignKey: 'id',
+    onDelete: 'CASCADE',
+});
+
+module.exports = {User, Posts, Comments}
