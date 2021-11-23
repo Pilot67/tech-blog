2f240e26afa03ec884e7e70fd35f3f470279a7a3
Stuart <86697483+Pilot67@users.noreply.github.com>
2021-11-23 16:08:33 +1100

    home route working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -8,9 +8,10 @@ router.get("/", async (req, res) => {
     const postsData = await Posts.findAll({
       include: [{ model: User, attributes: ["name"] }],
     });
-    const posts = postsData.map((posts) => posts.get({ plain: true }));
-    res.status(200).json(posts);
-
+    const posts = postsData.map((post) => post.get({ plain: true }));
+    //console.log({posts})
+    res.render("mainMenu", {posts});
+    //res.status(200).json(posts);
   } catch (err) {
     res.status(500).json(err);
   }
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,15 @@
+const {format, parseISO} = require('date-fns');
+
 module.exports = {
   format_date: (date) => {
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
+  formatDate: (date) => {
+      console.log(date);
+      //return format(parseISO(date), 'do MMM yyyy')
+      return format(date, 'do MMM yyyy')
+      return
+  }
+
 };
